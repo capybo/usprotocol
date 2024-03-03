a05315a9c8a2a8c271019d81edcd021f6e117f74
Andrew Shumel <155365407+capybo@users.noreply.github.com>
2024-03-03 15:49:56 +0300

    Accept command line argument for port

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,15 @@ mod server;
 mod commands;
 
 fn main() {
-    let server = server::Server::new(8080, 10);
-    server.start();
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <port>", args[0]);
+        std::process::exit(1);
+    }
+    let port: Result<u16, _> = args[1].parse();
+
+    if let (Ok(port)) = port {
+        let server = server::Server::new(port, 10);
+        server.start();
+    }
 }
